62b6abde9c12fd8b072be7e63a794db13dc32c42
George Mantakos <GeoMSKg@gmail.com>
2019-05-07 17:33:07 +0300

    config file in args, logging to file

--- gtfeed/__main__.py
+++ gtfeed/__main__.py
@@ -1,8 +1,8 @@
-import sys
 import re
 import yaml
 import logging
 import os
+import argparse
 from gtfeed import config
 from peewee import SqliteDatabase
 from pathlib import Path
@@ -15,37 +15,65 @@ def config_default(cfg: dict):
 	return cfg['general']['username'] == 'username' and cfg['general']['password'] == 'password'
 
 
+def setup_logging(logfile, level):
+	lv = {"debug": logging.DEBUG,
+	      "critical": logging.CRITICAL,
+	      "error": logging.CRITICAL,
+	      "warning": logging.CRITICAL,
+	      "info": logging.CRITICAL
+	      }
+	if level not in lv.keys():
+		log.error("loglevel should be one of \"%s\". Exiting..." % ",".join(lv.keys()))
+		exit(0)
+	log_formatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s]  %(message)s")
+	file_handler = logging.FileHandler(logfile)
+	file_handler.setFormatter(log_formatter)
+	console_handler = logging.StreamHandler()
+	console_handler.setFormatter(log_formatter)
+	root_logger = logging.getLogger()
+	root_logger.addHandler(console_handler)
+	root_logger.addHandler(file_handler)
+
+
 def main():
-	args = sys.argv[:]
-
-	gt_dir = Path.home() / ".gtfeed"
+	default_gt_dir = Path.home() / ".gtfeed"
 	cur_dir = Path(os.path.dirname(os.path.abspath(__file__)))
+	default_cfg_path = str(default_gt_dir / "config.yml")
 
-	if not os.path.exists(str(gt_dir)):
-		os.makedirs(str(gt_dir))
-	default_cfg_path = str(gt_dir / "config.yml")
-	if not os.path.exists(default_cfg_path):
-		copyfile(str(cur_dir / ".." / "config.yml"), default_cfg_path)
+	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument("-c", "--config", type=str, default=default_cfg_path,
+	                    help="Path to the gtfeed config file. If it does not exists a default config will be generated at that location if the --generate-config flag is set")
+	parser.add_argument("-g", "--generate-config", default=False, action="store_true",
+	                    help="If set and the specified configuration file does not exist, it will be generated")
+	args = parser.parse_args()
 
-	cfg = default_cfg_path
+	cfg = Path(args.config)
+	work_dir = cfg.parent
+	if args.generate_config:
+		if not os.path.exists(str(work_dir)):
+			os.makedirs(str(work_dir))
+		if not os.path.exists(str(cfg)):
+			copyfile(str(cur_dir / ".." / "config.yml"), str(cfg))
+
 	try:
-		with open(cfg, 'r') as ymlfile:
+		with open(str(cfg), 'r') as ymlfile:
 			config.cfg = yaml.load(ymlfile, Loader=yaml.Loader)
 			if config_default(config.cfg):
 				log.error("You have to change the default credentials in the configuration file. "
-				          "The default config file is in \"%s\"" % default_cfg_path)
+				          "Config file in use: \"%s\"" % str(cfg))
 				exit(0)
-			config.db = SqliteDatabase(config.cfg['general']['db_path'])
+			config.db = SqliteDatabase(str(work_dir / Path(config.cfg['general']['db_path'])))
+			logfile = str(work_dir / Path(config.cfg['logging']['logfile']))
+			level = config.cfg['logging']['loglevel']
+			setup_logging(logfile, level)
+
 	except FileNotFoundError as e:
-		log.error("Config file \"%s\" not found. Exiting..." % cfg)
+		log.error("Config file \"%s\" not found. Exiting..." % str(cfg))
 		exit(0)
 
 	from gtfeed.gtfeed import GT, TorrentModel  # this needs to be after the config initialization
 
 	filters = config.cfg['filtering']['filters']
-
-	logging.basicConfig()
-	logging.getLogger().setLevel(logging.DEBUG)
 
 	gt = GT(config.cfg['general']['username'], config.cfg['general']['password'])
 	if not gt.login():
